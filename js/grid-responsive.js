105b7523529621b470a41f1a0dcf0d2315270671
Ethan Shearer <ethanshearer1@gmail.com>
2017-06-29 16:11:02 +1000

    fixed mobile responsive issue

--- js/grid-responsive.js
+++ js/grid-responsive.js
@@ -3,6 +3,7 @@ var navResponsive = {
 		this.navbar = document.getElementById('nav-collapse')
 		this.toggleBtn = document.getElementById('icon');
 		this.dropdownbtns = this.findDropDwnBtns();
+		this.toggleBtn.onclick = this.navExpand.bind(this);
 
 	},
 	navExpand: function(){
