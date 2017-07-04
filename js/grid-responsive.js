8a077c75c070cd473ac5eff87bdbb51d43f70748
Ethan Shearer <ethanshearer1@gmail.com>
2017-07-04 12:25:57 +1000

    working on dropdowns

--- js/grid-responsive.js
+++ js/grid-responsive.js
@@ -15,13 +15,16 @@ var navResponsive = {
 		};
 	},
 	openSubMenu: function(x){
-		for(var i = 0; i < x.children.length; i++){
-			if(x.children[i].tagName == 'UL'){
-				console.log('shit');
-				if(x.children[i].hidden == true){
-					x.children[i].hidden = false;
+		var pressed = x.target.parentNode;
+		console.log(pressed);
+		for(var i = 0; i < pressed.children.length; i++){
+			if(pressed.children[i].tagName == 'UL'){
+				if(pressed.children[i].className =='submenu'){
+					pressed.children[i].className += ' open';
+					console.log(pressed.children[i]);
 				}else{
-					x.children[i].hidden = true;
+					pressed.children[i].className = 'submenu';
+					console.log(pressed.children[i]);
 				}
 			}
 		}
@@ -32,18 +35,22 @@ var navResponsive = {
 		for (var i = 0; i < navlinks.length; i++){
 			var ul = navlinks[i].getElementsByTagName('ul');
 			if(ul.length > 0){
+				var sibs = ul[0].parentNode.children;
+				var a = sibs[0];
+				console.log(a);
+				var myTag = document.createElement('i');
+				myTag.className = 'arrow down';
+				a.appendChild(myTag);
 				for(var x= 0; x < ul.length; x++){
 					ul[x].className = 'submenu';
-					ul[x].hidden = true;
-					ul[x].parentNode.className = 'dropdown';
+					//ul[x].parentNode.className += ' dropdown';
 					var drpBtn = ul[x].parentNode;
-					console.log(drpBtn);
-					drpBtn.onclick = this.openSubMenu.bind(this,drpBtn);
+					drpBtn.onclick = this.openSubMenu.bind(this);
 					btns.push(drpBtn);
 				}
 			}
 		}
 		return btns;
-	},
+	}
 }
 navResponsive.init();
