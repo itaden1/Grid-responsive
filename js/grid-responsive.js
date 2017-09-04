ea9c9c36ff31fe02e3df7813d26eacbaa2f23d1b
Ethan Shearer <itaden@hotmail.com>
2017-09-04 14:32:10 +1000

    fixed drop down animation on nav

--- js/grid-responsive.js
+++ js/grid-responsive.js
@@ -1,60 +1,25 @@
 var navResponsive = {
 	init: function(){
-		this.navbar = document.getElementsByClassName('nav-collapse')
-		this.toggleBtn = document.getElementsByClassName('icon');
+		this.navbar = document.getElementById('nav-collapse')
+		this.toggleBtn = document.getElementById('icon');
 		//this.dropdownbtns = this.findDropDwnBtns();
-		for(var i=0;i<this.toggleBtn.length;i++){
-			this.toggleBtn[i].onclick = this.navExpand.bind(target);
-		}
+	
+		this.toggleBtn.onclick = this.navExpand.bind(this);
+		
 
 	},
-	navExpand: function(target){
-		console.log('sup');
-		var navbar = target;
-		if (navbar.className === 'nav'){
-			navbar.className += ' responsive';
+	navExpand: function(){
+		var navbar = this.navbar;
+		var height = document.getElementById('nav-collapse').clientHeight;
+		console.log(height);
+		if (!navbar.classList.contains('responsive')){
+			navbar.classList.add('responsive');
+			navbar.classList.remove('close');
 		}else{
-			navbar.className = 'nav';
+			navbar.classList.remove('responsive');
+			navbar.classList.add('close');
 
 		}
-	},
-	openSubMenu: function(x){
-		var pressed = x.target.parentNode;
-		console.log(pressed);
-		for(var i = 0; i < pressed.children.length; i++){
-			if(pressed.children[i].tagName == 'UL'){
-				if(pressed.children[i].className =='submenu'){
-					pressed.children[i].className += ' open';
-					console.log(pressed.children[i]);
-				}else{
-					pressed.children[i].className = 'submenu';
-					console.log(pressed.children[i]);
-				}
-			}
-		}
-	},
-	findDropDwnBtns: function(){
-		var navlinks = this.navbar.getElementsByTagName('li');
-		var btns = [];
-		for (var i = 0; i < navlinks.length; i++){
-			var ul = navlinks[i].getElementsByTagName('ul');
-			if(ul.length > 0){
-				var sibs = ul[0].parentNode.children;
-				var a = sibs[0];
-				console.log(a);
-				var myTag = document.createElement('i');
-				myTag.className = 'arrow down';
-				a.appendChild(myTag);
-				for(var x= 0; x < ul.length; x++){
-					ul[x].className = 'submenu';
-					//ul[x].parentNode.className += ' dropdown';
-					var drpBtn = ul[x].parentNode;
-					drpBtn.onclick = this.openSubMenu.bind(this);
-					btns.push(drpBtn);
-				}
-			}
-		}
-		return btns;
 	}
 }
 navResponsive.init();
