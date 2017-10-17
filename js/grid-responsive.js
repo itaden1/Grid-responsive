2d67768b6db651594cab6c9c4ba67f4f958ba614
Ethan Shearer <itaden@hotmail.com>
2017-10-17 17:02:06 +1100

    drop down menu

--- js/grid-responsive.js
+++ js/grid-responsive.js
@@ -1,6 +1,11 @@
-function navResponsive(bar,btn){
-	this.navbar = document.getElementById(bar);
-	this.toggleBtn = document.getElementById(btn);
+function navResponsive(){
+
+	this.init = function(bar,btn){
+		this.navbar = document.getElementById(bar);
+		this.toggleBtn = document.getElementById(btn);
+		this.toggleBtn.addEventListener('click',this.navExpand.bind(this));
+	}
+
 	this.navExpand = function(){
 		console.log(this);
 		var navbar = this.navbar;
@@ -12,13 +17,28 @@ function navResponsive(bar,btn){
 			navbar.classList.add('close');
 		}
 	}
-	this.toggleBtn.addEventListener('click',this.navExpand.bind(this))
-	
-	
+}
+
+function dropDown(){
+	this.init = function(btn,menu){
+		this.btn = document.getElementById(btn);
+		this.menu = document.getElementById(menu);
+		this.btn.addEventListener('click', this.expand.bind(this));
+	}
+	this.expand = function(){
+		this.menu.classList.toggle('open');
+	}
 }
 
 
 // tests
 // on a live site these will be in seperate script
-var myNav = new navResponsive('nav-collapse','icon');
-var myNav3 = new navResponsive('nav-collapse3','icon3');
+var myNav = new navResponsive();
+myNav.init('nav-collapse','icon');
+var myNav3 = new navResponsive();
+myNav3.init('nav-collapse3','icon3');
+
+var myDropDwn = new dropDown();
+myDropDwn.init('drop-btn','drop-menu');
+var drop2 = new dropDown();
+drop2.init('drop-btn2','drop-menu2');
